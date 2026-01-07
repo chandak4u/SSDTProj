bc3af4ae4532a1bc5a4dc336f212437e7377fe25
chandak4u <chandak4u@gmail.com>
2026-01-07 18:12:02 +0530

    Changed server name
    Changed server name

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "Waiting for SQL Server..."
 sleep 30s
 
 for i in {1..30}; do
-    if /opt/mssql-tools18/bin/sqlcmd -S APT045CD119FX9N\MSSQLSERVER2025 -U sa -P "${MSSQL_SA_PASSWORD}" -C -Q "SELECT 1" &> /dev/null; then
+    if /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "${MSSQL_SA_PASSWORD}" -C -Q "SELECT 1" &> /dev/null; then
         echo "SQL Server ready!"
         break
     fi
@@ -19,7 +19,7 @@ echo "Deploying database..."
 #!
 /opt/sqlpackage/sqlpackage /Action:Publish \
     /SourceFile:/usr/src/app/AdventureWorks2019.dacpac \
-    /TargetServerName:APT045CD119FX9N\MSSQLSERVER2025 \
+    /TargetServerName:localhost \
     /TargetDatabaseName:AdventureWorks2019 \
     /TargetUser:sa \
     /TargetPassword:"${MSSQL_SA_PASSWORD}" \
